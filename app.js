56f8be8b8d0634dc426d85df4afe36ebb3d3ecd3
abimbolataofeek <abimbolaadewale9999@gmail.com>
2022-01-11 00:18:47 +0100

    level one security

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const ejs = require("ejs");
 const bodyparser = require("body-parser");
-const { resolveSoa } = require("dns");
+const mongoose = require("mongoose");
 
 
 const app = express();
@@ -10,20 +10,62 @@ app.use(express.static("public"));
 app.set('view engine', 'ejs');
 app.use(bodyparser.urlencoded({ extended: true }));
 
-app.get("/", function(req, res) {
+mongoose.connect("mongodb://localhost:27017/userDB", { useNewUrlParser: true });
+
+const userSchema = {
+    email: String,
+    password: String
+};
+
+const User = new mongoose.model("User", userSchema);
+
+app.get("/", function (req, res) {
     res.render("home");
-    
+
 });
 
-app.get("/login", function(req, res) {
+app.get("/login", function (req, res) {
     res.render("login");
-    
+
 });
 
-app.get("/register", function(req, res) {
+app.get("/register", function (req, res) {
     res.render("register");
-    
+
 });
+
+
+app.post("/register", function (req, res) {
+    const newUser = new User({
+        email: req.body.username,
+        password: req.body.password
+    });
+    newUser.save(function (err) {
+        if (err) {
+            console.log(err);
+        } else {
+            res.render("secrets")
+        }
+    });
+});
+
+app.post("/login", function (req, res) {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    User.findOne({ email: username }, function (err, foundUser) {
+        if (err) {
+            console.log(err);
+        } else {
+            if (foundUser) {
+                if (foundUser.password === password) {
+                    res.render("secrets");
+                }
+            }
+        }
+    })
+})
+
 
 app.listen(9000, function () {
     console.log("Server started on port 9000");
